7e4801494485dd01ff44b30b08d39bba5ac1efc4
Wojciech Bury <wzbury@wp.pl>
2024-11-05 15:14:36 +0100

    Initial commit
    This includes basic files generated by InteliJ as well as my attempt at parsing pom.xml using Apache Xerces.

---
+++ src/main/java/wb/Main.java
@@ -0,0 +1,7 @@
+package wb;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/wb/POMParser.java
@@ -0,0 +1,4 @@
+package wb;
+
+public class POMParser {
+}
